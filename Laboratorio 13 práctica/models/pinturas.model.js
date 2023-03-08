34697ecd708d0858c20cd968288cd23623bcf631
FridaBG2002 <A01708633@itesm.mx>
2023-03-08 12:02:53 -0600

    actualizar lab 14

--- Laboratorio 13 práctica/models/pinturas.model.js
+++ Laboratorio 13 práctica/models/pinturas.model.js
@@ -5,12 +5,14 @@ const pinturas = [
         imagen: "https://www.albat.com/__export/1605215759731/sites/debate/img/2020/11/12/trevor_bauer.jpg_976912859.jpg", 
         fecha: "ayer :D" ,
     }, 
+
     {
         nombre: "Bauer en la comida" ,
         autor: "Frix BG" , 
         imagen: "https://m.media-amazon.com/images/I/61BiQ+UsfGL._AC_SL1200_.jpg", 
         fecha: "05/06/999" ,
     }, 
+    
     { 
         nombre: "Bauer en el desayuno" ,
         autor: "Ranix RG" , 
@@ -25,10 +27,10 @@ module.exports = class Pintura {
 
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
     constructor(nuevo_pintura) {
-        this.nombre = nuevo_pintura.nombre || 'Firulais';
-        this.autor = nuevo_pintura.autor || 'Delmer';
+        this.nombre = nuevo_pintura.nombre || 'Obra 1';
+        this.autor = nuevo_pintura.autor || 'Autor 1';
         this.imagen = nuevo_pintura.imagen || 'https://bulma.io/images/placeholders/1280x960.png';
-        this.fecha = nuevo_pintura.fecha || 'Un perro muy cool';
+        this.fecha = nuevo_pintura.fecha || '01/01/2002';
     }
 
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
